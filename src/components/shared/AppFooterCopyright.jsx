f6cd44c38e1029792424a5fabb462dd162d653fc
Eden Pearson <pearsoneden@gmail.com>
2023-10-05 11:37:28 +1300

    personalise footer

--- src/components/shared/AppFooterCopyright.jsx
+++ src/components/shared/AppFooterCopyright.jsx
@@ -1,26 +1,37 @@
 function AppFooterCopyright() {
-	return (
-		<div className="font-general-regular flex justify-center items-center text-center">
-			<div className="text-lg text-ternary-dark dark:text-ternary-light">
-				&copy; {new Date().getFullYear()}
-				<a
-					href="https://github.com/realstoman/react-tailwindcss-portfolio"
-					target="__blank"
-					className="hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
-				>
-					React & Tailwind CSS Portfolio
-				</a>
-				.
-				<a
-					href="https://stoman.me"
-					target="__blank"
-					className="text-secondary-dark dark:text-secondary-light font-medium uppercase hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
-				>
-					Stoman
-				</a>
-			</div>
-		</div>
-	);
+  return (
+    <div className="font-general-regular flex justify-center items-center text-center">
+      <div className="text-lg text-ternary-dark dark:text-ternary-light">
+        © {new Date().getFullYear()}
+        <a
+          href="https://github.com/eden-pearson/personal-portfolio"
+          target="__blank"
+          className=" dark:text-secondary-light font-medium hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
+        >
+          Eden Pearson Portfolio{' '}
+        </a>
+        <div className="mt-4 text-gray-400">
+          Thanks
+          <a
+            href="https://stoman.me"
+            target="__blank"
+            className="text-gray-400 dark:text-secondary-light font-medium hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
+          >
+            Stoman{' '}
+          </a>
+          for the open-source{' '}
+          <a
+            href="https://github.com/realstoman/react-tailwindcss-portfolio"
+            target="__blank"
+            className="text-gray-400 dark:text-secondary-light font-medium hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
+          >
+            template
+          </a>
+          !
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default AppFooterCopyright;
+export default AppFooterCopyright
